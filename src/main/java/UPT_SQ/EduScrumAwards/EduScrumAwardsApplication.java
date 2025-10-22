10d8871d0ce803f144e3ed6303d37fe1ba55807a
Anastasiia <n21koketka@gmail.com>
2025-10-22 17:15:46 +0300

    update

--- src/main/java/UPT_SQ/EduScrumAwards/EduScrumAwardsApplication.java
+++ src/main/java/UPT_SQ/EduScrumAwards/EduScrumAwardsApplication.java
@@ -1,6 +1,18 @@
 package UPT_SQ.EduScrumAwards;
 
-public class EduScrumAwardsApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "UPT_SQ.EduScrumAwards.repository")
+@EntityScan(basePackages = "UPT_SQ.EduScrumAwards.model")
+public class EduScrumAwardsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EduScrumAwardsApplication.class, args);
+    }
 
 }
