34b003e7303781ef3a84278858b4e52df6b521d7
Fatimasania <fatimasania871@gmail.com>
2025-10-28 22:28:21 +0000

    [SPRINT1] create Sprint and Goal

---
+++ src/main/java/UPT_SQ/EduScrumAwards/model/Sprint.java
@@ -0,0 +1,50 @@
+package UPT_SQ.EduScrumAwards.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Sprint {
+    private int sprintId;
+    private Date startDate;
+    private Date endDate;
+    private ArrayList<Goal> goals;
+
+    public Sprint(int sprintId, Date startDate, Date endDate, ArrayList<Goal> goals) {
+        this.sprintId = sprintId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.goals = goals;
+    }
+
+    public int getSprintId() {
+        return sprintId;
+    }
+
+    public void setSprintId(int sprintId) {
+        this.sprintId = sprintId;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public ArrayList<Goal> getGoals() {
+        return goals;
+    }
+
+    public void setGoals(ArrayList<Goal> goals) {
+        this.goals = goals;
+    }
+}
