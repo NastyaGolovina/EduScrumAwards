8c5d6ab9fe9efa409eae72f294ccefd96d793034
Anastasiia <n21koketka@gmail.com>
2025-12-05 21:10:57 +0200

    [SPRINT4] update AwardController

--- src/main/java/UPT_SQ/EduScrumAwards/controller/AwardController.java
+++ src/main/java/UPT_SQ/EduScrumAwards/controller/AwardController.java
@@ -1,12 +1,13 @@
 package UPT_SQ.EduScrumAwards.controller;
 
-import UPT_SQ.EduScrumAwards.model.Award;
-import UPT_SQ.EduScrumAwards.model.Global;
+import UPT_SQ.EduScrumAwards.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.management.relation.Role;
+import java.util.ArrayList;
 import java.util.List;
 
 //@CrossOrigin(origins = "http://localhost:63342")
@@ -25,6 +26,28 @@ public class AwardController {
     @GetMapping("/all")
     public List<Award> getAllAwards() {
         return global.getAwards();
+    }
+
+
+    @GetMapping("/projects/all")
+    public List<Project> getAllProject() {
+        List<Project> projects = new ArrayList<>();
+        for(Course course : global.getCourses()) {
+            projects.addAll(course.getProjects());
+        }
+        return projects;
+    }
+
+
+    @GetMapping("/teachers/all")
+    public List<Teacher> getAllTeachers() {
+        List<Teacher> teachers = new ArrayList<>();
+        for(User user : global.getUsers()) {
+            if(UserRole.TEACHER == user.getRole()) {
+                teachers.add((Teacher)user);
+            }
+        }
+        return teachers;
     }
 
 
